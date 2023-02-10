1bb59710179d5de9076be1c9c1df9d0c4725ad23
James Joy <joyja@jarautomation.io>
2023-02-10 03:27:40 +0000

    updated main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import * as THREE from "three";
+	import { onMount } from "svelte";
+
+  onMount(() => {
+    const scene = new THREE.Scene()
+
+    const camera = new THREE.PerspectiveCamera(
+      50,
+      window.innerWidth / window.innerHeight,
+      1,
+      1000
+    )
+    camera.position.z = 96
+    const canvas = document.getElementById('threed')!
+    const renderer = new THREE.WebGLRenderer({
+      canvas,
+      antialias: true
+    })
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    document.body.appendChild(renderer.domElement)
+  })
+</script>
+<div>
+  <canvas id="threed"></canvas>
+</div>
